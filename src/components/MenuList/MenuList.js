1d60e65d4040c492ce23bdba1a484607cef729b6
ziqian pan <ziqianpan1998@gmail.com>
2023-01-29 15:42:06 -0500

    Show no result for search

--- src/components/MenuList/MenuList.js
+++ src/components/MenuList/MenuList.js
@@ -2,29 +2,49 @@ import React, { useState } from "react";
 import MenuItem from "../MenuItem/MenuItem";
 
 export default function MenuList({ fullMenu }) {
-    const [searchInput, setSearchInput] = useState("");
-  
-    const handleChange = (e) => {
-        setSearchInput(e.target.value);
-      };
+  const [searchInput, setSearchInput] = useState("");
 
-      let filtedMenu = fullMenu;
+  const handleChange = (e) => {
+    setSearchInput(e.target.value);
+  };
 
-      if (searchInput) {
-        filtedMenu = fullMenu.filter((item) => {
-         return item.name.toLowerCase().includes(searchInput.toLowerCase()) ||
-         item.shortDescription.toLowerCase().includes(searchInput.toLowerCase());
-        });
-      }
+  let filtedMenu = fullMenu;
 
+  if (searchInput) {
+    filtedMenu = fullMenu.filter((item) => {
+      return (
+        item.name.toLowerCase().includes(searchInput.toLowerCase()) ||
+        item.shortDescription.toLowerCase().includes(searchInput.toLowerCase())
+      );
+    });
+  }
 
+  const renderContent = () => {
+    if (filtedMenu.length === 0) {
+      //if no result
+      return (
+        <div>No result for {searchInput}</div>
+      ) 
+     
+    } else {
+      return (
+        <div>
+          {filtedMenu.map((item) => (
+            <MenuItem key={item.id} item={item} />
+          ))}
+        </div>
+      );
+    }
+  };
 
   return (
     <div>
-      <input type="text" placeholder="Search for Item" onChange={handleChange}/>
-      {filtedMenu.map((item) => (
-        <MenuItem key={item.id} item={item} />
-      ))}
+      <input
+        type="text"
+        placeholder="Search for Item"
+        onChange={handleChange}
+      />
+      {renderContent()}
     </div>
   );
 }
